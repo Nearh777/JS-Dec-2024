5844bf938b598c5ef68e02c201e5f7f0dffb5931
Nearh777 <nearh777@gmail.com>
2025-02-03 23:17:49 +0200

    finish lesson 1

---
+++ Lesson1/Lesson_1_4/index.js
@@ -0,0 +1,12 @@
+//За допомогою 3х різних prompt() отримати 3 слова
+// які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+
+
+let firstName = prompt("Введіть Ваше ім'я");
+console.log(firstName);
+let middleName = prompt("Введіть Ваше По-Батькові");
+console.log( middleName);
+let age = prompt("Введіть Ваш вік")
+
+console.log( age + ' ' + 'років');
