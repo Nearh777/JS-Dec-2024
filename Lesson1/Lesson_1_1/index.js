ced7d44a1476e6d48bb310275b573ef71608792f
Nearh777 <nearh777@gmail.com>
2025-02-03 22:03:56 +0200

    start lesson 1.2

---
+++ Lesson1/Lesson_1_1/index.js
@@ -0,0 +1,31 @@
+<!-- 1. Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false. -->
+<!-- Вивести кожну змінну за допомогою: console.log -->
+
+
+let a = 'hello';
+let b = 'owu';
+let c = 'com';
+let d = 'ua';
+let e = 1;
+let f = 10;
+let t = -999;
+let r = 123;
+let k = 3.14;
+let j = 2.7;
+let h = 16;
+let s = true;
+let l = false;
+
+console.log('Змінна a:', a);
+console.log('Змінна b:', b);
+console.log('Змінна c:', c);
+console.log('Змінна d:', d);
+console.log('Змінна e:', e);
+console.log('Змінна f:', f);
+console.log('Змінна t:', t);
+console.log('Змінна r:', r);
+console.log('Змінна k:', k);
+console.log('Змінна j:', j);
+console.log('Змінна h:', h);
+console.log('Змінна s:', s);
+console.log('Змінна l:', l);
---
+++ Lesson1/Lesson_1_2/index.js
@@ -0,0 +1,8 @@
+<!-- 2. Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. -->
+<!-- З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації) -->
+
+let firstName = 'Сергій';
+let middleName = 'Дмитрович';
+let lastName = 'Трефель';
+
+console.log(lastName + ' ' + firstName + ' ' + middleName);
