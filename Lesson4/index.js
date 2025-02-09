7cc6150cfda4c0e37950f9a19e9d7d67b83010b4
Nearh777 <nearh777@gmail.com>
2025-02-09 17:20:13 +0200

    HW №4.3

--- Lesson4/index.js
+++ Lesson4/index.js
@@ -3,9 +3,26 @@
 //#I2XsG6f
 //4.1 - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
 
-function squareOfRectangle(a, b) {
-const result = a * b;
-   return  console.log(`Площа прямокутника дорівнює S = ${result}`);
-}
+//function squareOfRectangle(a, b) {
+// const result = a * b;
+//    return  console.log(`Площа прямокутника дорівнює S = ${result}`);
+// }
+//
+// squareOfRectangle(7, 8);
 
-squareOfRectangle(7, 8);
+
+// #ETGAxbEn8l
+//4.2 - створити функцію яка обчислює та повертає площу кола з радіусом r
+
+
+//function squareCircle(r) {
+//     const result = Math.PI * Math.pow(r, 2)
+//     return console.log(`Площа кола дорівнює S = ${result}`);
+// }
+//
+//
+// squareCircle(2);
+
+//#Mbiz5K4yFe7
+//4.3 - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
+
