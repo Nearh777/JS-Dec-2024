2e2c719a116964bc0639376c9caaaf87ea332c9f
Nearh777 <nearh777@gmail.com>
2025-02-09 20:25:59 +0200

    HW №4.9

--- Lesson4/index.js
+++ Lesson4/index.js
@@ -58,3 +58,84 @@
 
 //#hOL6126
 // 4.6- створити функцію яка створює ul з трьома елементами li та виводить його через document.write. Текст li задати через аргумент всім однаковий
+
+
+//function outputList(text) {
+// return document.write(`<ul>
+// <li>${text}</li>
+// <li>${text}</li>
+// <li>${text}</li>
+// </ul>`)
+// }
+//
+// outputList('lorem home!');
+
+
+//#0Kxco1edSN
+//4.7 - створити функцію яка створює ul з  елементами li.
+// Текст li задати через аргумент всім однаковий.
+// Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл) та виводить його через document.write
+
+//function outputList(text, quantity) {
+//     document.write(`<ul>`);
+//     for (let i = 1; i <= quantity; i += 1) {
+//        document.write(`<li>${text}</li>`);
+//     }
+//     document.write(`</ul>`);
+//
+// }
+//
+// outputList('Hello!', 15);
+
+
+//#gEFoxMMO
+// 4.8- створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві),
+// та будує для них список (ul li) та виводить його через document.write
+
+//function arrayItem(arr) {
+//     document.write(`<ul>`);
+//     for (let i = 0; i < arr.length; i += 1) {
+//         document.write(`<li>${arr[i]}</li>`);
+//     }
+//
+//     document.write(`</ul>`);
+// }
+//
+// arrayItem([1, 'hello!', true, 234, 'milk', false, 546, 'tomato']);
+
+//#bovDJDTIjt
+//4.9 - створити функцію яка приймає масив об'єктів з наступними полями id,name,age
+// та виводить їх в документ. Для кожного об'єкту окремий блок.
+
+//function objectItem(object) {
+//     document.write(`<ul>`);
+//     for (const item of object) {
+//         document.write(`<li>${item.id} ${item.name} ${item.age}</li>`)
+//     }
+//     document.write(`</ul>`);
+// }
+//
+// let users = [
+//     {
+//         id: 1,
+//         name: 'Serhii',
+//         age: 45
+//     },
+//     {
+//         id: 2,
+//         name: 'Jon',
+//         age: 25
+//     },
+//     {
+//         id: 3,
+//         name: 'Volodimir',
+//         age: 35
+//     },
+//
+// ];
+//
+// objectItem(users);
+
+//#pghbnSB
+//4.10 - створити функцію яка повертає найменьше число з масиву
+
