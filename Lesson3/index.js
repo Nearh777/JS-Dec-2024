4355855e8801a40063962ceca12a31bca5f65ba7
Nearh777 <nearh777@gmail.com>
2025-02-09 12:09:26 +0200

    start HW №3

---
+++ Lesson3/index.js
@@ -0,0 +1,61 @@
+// 3.1 - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+
+'use strict';
+
+
+//for (let i = 1; i < 11; i += 1) {
+//     document.write(`<div><h3>Lorem ipsum dolor.</h3></div>`)
+// }
+
+//3.2 - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+
+//for (let i = 1; i < 11; i += 1) {
+//     document.write(`<div>
+// <h3>Lorem ipsum dolor. Index - ${i}</h3>
+// </div>`)
+// }
+
+//3.3 - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
+
+
+//let i = 1;
+//
+//  while ( i < 21) {
+//      document.write(`<h1>Lorem ipsum dolor.</h1>`);
+//      i += 1;
+//  }
+
+//3.3 - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
+
+//let i = 1;
+//
+// while (i < 21) {
+//     document.write(`<h1>Lorem ipsum dolor. Index - ${i}</h1>`);
+//     i += 1;
+// }
+
+
+
+//3.4 - Використовуючи данні з масиву, за допомоги document.write та циклу
+// побудувати структуру по шаблону
+// Масив:
+//
+// let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+//
+// ШАБЛОН:
+//  <ul>
+//     <li>ITEM OF ARRAY</li>
+//     <!--
+//         і тд інші об'єкти масиву
+//          ...
+//          ...
+//          ...
+//     -->
+// </ul>
+//
+// замість 'ITEM OF ARRAY' підставити елемент з масиву щоб получився цілий список з даними з масиву
+
+let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+
+
+
