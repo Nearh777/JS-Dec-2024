f0c59745863940c7f6d58cca83002e0e8320a510
Nearh777 <nearh777@gmail.com>
2025-02-15 11:59:55 +0200

    start HW №5

--- Lesson4/index.js
+++ Lesson4/index.js
@@ -4,8 +4,8 @@
 //4.1 - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
 
 //function squareOfRectangle(a, b) {
-// const result = a * b;
-//    return  console.log(`Площа прямокутника дорівнює S = ${result}`);
+
+//    return  a * b;
 // }
 //
 // squareOfRectangle(7, 8);
