a52280e03341fc3ab13a234607022fa1bff2bec9
Nearh777 <nearh777@gmail.com>
2025-02-16 23:33:39 +0200

    task HW №7.6

--- Lesson7/index.js
+++ Lesson7/index.js
@@ -128,3 +128,39 @@
 //     -- increaseMaxSpeed (newSpeed) - яка підвищує значення максимальної швидкості на значення newSpeed
 //     -- changeYear (newValue) - змінює рік випуску на значення newValue
 //     -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
+
+
+//function Car(model, producer, year, maxSpeed, engineVolume) {
+//     this.model = model;
+//     this.producer = producer;
+//     this.year = year;
+//     this.maxSpeed = maxSpeed;
+//     this.engineVolume = engineVolume;
+//     this.drive = function () {
+//         console.log(`їдемо зі швидкістю ${this.maxSpeed} на годину`);
+//     };
+//     this.info = function () {
+//         for (const key in this) {
+//             console.log(key, this[key]);
+//         }
+//     };
+//     this.increaseMaxSpeed = function (newSpeed) {
+//         if(newSpeed > 0) this.maxSpeed = this.maxSpeed + newSpeed;
+//     };
+//     this.changeYear = function (newYear) {
+//         if(newYear > 1885) this.year = newYear;
+//     };
+//     this.addDriver = function (driverObject) {
+//         if(driverObject) this.driver = driverObject;
+//     }
+// };
+//
+// const car = new Car('Camry', 'Toyota', '2023', 200, '2353');
+//
+// console.log(car);
+// car.drive();
+// car.info();
+// car.increaseMaxSpeed(100);
+// car.changeYear(2015);
+// car.addDriver({});
+// console.log(car);
