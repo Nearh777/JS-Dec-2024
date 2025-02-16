331170bfa7ee72d258ba985c9726ed364bb415a9
Nearh777 <nearh777@gmail.com>
2025-02-16 12:12:12 +0200

    HW №6.8

--- Lesson6/index.js
+++ Lesson6/index.js
@@ -112,6 +112,24 @@
 //  -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
 //  -- за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
 //  =========================
+
+
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+let sortCourses = coursesAndDurationArray.sort((a, b) => b.monthDuration - a.monthDuration);
+console.log(sortCourses);
+
+let filterCourses = coursesAndDurationArray.filter((course) => {if(course.monthDuration > 5) return course; })
+console.log(filterCourses);
+
+
 //  #bolvdlhP
 //  описати колоду карт (від 6 до туза без джокерів)
 //  - знайти піковий туз
