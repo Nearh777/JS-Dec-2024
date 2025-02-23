9b7f730285bab140236553c16f59454806adce6a
Nearh777 <nearh777@gmail.com>
2025-02-23 23:28:11 +0200

    start HW № 10.2

--- Lesson10/index.js
+++ Lesson10/index.js
@@ -1,4 +1,39 @@
 'use strict';
 
 
+//#sH8c4er
+// 10.1 - Створити довільний елемент з id = text та створити кнопку.
+// Використовуючи JavaScript, зробіть так, щоб при натисканні на кнопку зникав елемент з id="text".
+
+
+// let div = document.createElement('div');
+// div.setAttribute('id', 'text');
+// div.classList.add('box');
 //
+// let button = document.createElement('button');
+// button.classList.add('button');
+//
+// document.body.append(div, button);
+//
+//
+// const handleClick = () => {
+//     div.classList.toggle('hidden');
+//
+// }
+//
+// button.addEventListener('click', handleClick);
+
+
+
+
+// const buttons = document.getElementsByTagName('button');
+// const button = buttons[0];
+//
+// button.onclick = function () {
+//     document.getElementById('text').classList.toggle('hidden');
+// }
+
+
+//#j693ca8
+// 10.2 - створити інпут який приймає вік людини та кнопку яка підтверджує дію.
+// При натисканні на кнопку зчитати інформацію з інпуту та перевірити вік чи меньше він ніж 18, та повідомити про це користувача
