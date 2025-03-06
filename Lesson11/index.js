76a59373b916bdc9f10f6ca3314b0a2fc9c08da3
Nearh777 <nearh777@gmail.com>
2025-03-06 23:51:16 +0200

    finish HW № 11

--- Lesson11/index.js
+++ Lesson11/index.js
@@ -64,90 +64,57 @@
 // #whXxOBlYS0H
 // 11.2 - взяти https://dummyjson.com/docs/recipes та вивести інформацію про всі рецепти. Інгредієнти повинні бути список під час відображення.
 
-const recipesDiv = document.getElementById('recipes');
-
-
-fetch('https://dummyjson.com/recipes')
-    .then(res => res.json())
-    .then(({recipes}) => {
-        console.log(recipes);
-
-
-        for (const recipe of recipes) {
-
-            const div = document.createElement('div');
-
-
-            div.classList.add('recipe-container');
-            const divWithInfo = document.createElement('div');
-
-            divWithInfo.textContent = `
-            caloriesPerServing: ${recipe.caloriesPerServing},
-            cookTimeMinutes: ${recipe.cookTimeMinutes},
-            cuisine: ${recipe.cuisine},
-            difficulty: ${recipe.difficulty},
-            id: ${recipe.id},
-            mealType: ${recipe.mealType},
-            name: ${recipe.name},
-            prepTimeMinutes: ${recipe.prepTimeMinutes},
-            rating: ${recipe.rating},
-            reviewCount: ${recipe.reviewCount},
-            servings: ${recipe.servings},
-            tags: ${recipe.tags},
-            userId: ${recipe.userId},      
-            
-            `
-
-            const ulIngredients = document.createElement('ul');
-            ulIngredients.classList.add('list-ingredients');
-            for (const ingredient of recipe.ingredients) {
-            const li = document.createElement('li');
-                li.classList.add('list-ingredients-item');
-            li.innerText = ingredient;
-                ulIngredients.appendChild(li);
-            }
-
-            const ulInstructions = document.createElement('ul');
-            ulInstructions.classList.add('list-ulInstructions');
-            for (const instruction of recipe.instructions) {
-                const li = document.createElement('li');
-                li.classList.add('list-instructions-item');
-                li.innerText = instruction;
-                ulInstructions.appendChild(li);
-            }
-
-
-
-            const img = document.createElement('img');
-            img.src = recipe.image;
-            div.append(ulInstructions, ulIngredients, divWithInfo, img);
-            recipesDiv.appendChild(div);
-        }
-
-
-
-// image: "https://cdn.dummyjson.com/recipe-images/1.webp"
-// ingredients: (6) ['Pizza dough', 'Tomato sauce', 'Fresh mozzarella cheese', 'Fresh basil leaves', 'Olive oil', 'Salt and pepper to taste']
-// instructions: (6) ['Preheat the oven to 475°F (245°C).', 'Roll out the pizza dough and spread tomato sauce evenly.', 'Top with slices of fresh mozzarella and fresh basil leaves.', 'Drizzle with olive oil and season with salt and pepper.', 'Bake in the preheated oven for 12-15 minutes or until the crust is golden brown.', 'Slice and serve hot.']
-
-        //         for (const cart of carts) {
-//             const div = document.createElement('div');
-//             div.classList.add('cart-container');
-//             const divWithInfo = document.createElement('div');
+// const recipesDiv = document.getElementById('recipes');
+// const url = new URL('https://dummyjson.com/recipes');
+// url.searchParams.set('limit', '50');
 //
-//             divWithInfo.textContent = `
-//             "total": ${cart.total},
-//             "discountedTotal": ${cart.discountedTotal},
-//             "userId": ${cart.userId},
-//             "totalProducts": ${cart.totalProducts},
-//             "totalQuantity": ${cart.totalQuantity}
+// fetch(url)
+//     .then(res => res.json())
+//     .then(({recipes}) => {
+//         console.log(recipes);
 //
-//             `;
-
-    });
-
-
-
+//
+//         for (const recipe of recipes) {
+//             const recipeDiv = document.createElement('div');
+//
+//             for (const recipeKey in recipe) {
+//                 if (Array.isArray(recipe[recipeKey])) {
+//                     const arrayTitleDiv = document.createElement('div');
+//                     const title = document.createElement('h3');
+//                     title.innerText = `${recipeKey}:`;
+//                     const list = document.createElement('ol');
+//                     const array = recipe[recipeKey];
+//                     for (const item of array) {
+//                         console.log(recipeKey);
+//                         const listItem = document.createElement('li');
+//                         listItem.innerText = item;
+//                         list.appendChild(listItem);
+//                     }
+//                     arrayTitleDiv.append(title, list);
+//                     recipeDiv.appendChild(arrayTitleDiv);
+//                 } else {
+//                     if (recipeKey !== 'image') {
+//
+//                         const keyDiv = document.createElement('div');
+//                         keyDiv.innerText = `${recipeKey}: ${recipe[recipeKey]}`;
+//                         recipeDiv.appendChild(keyDiv);
+//                     }
+//                 }
+//
+//
+//             }
+//             const img = document.createElement('img');
+//             img.src = recipe.image;
+//             recipeDiv.appendChild(img);
+//
+//             recipesDiv.appendChild(recipeDiv);
+//         }
+//
+//
+//     });
+//
+//
+//
 
 
 
